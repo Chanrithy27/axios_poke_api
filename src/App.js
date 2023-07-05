24756cd324420d2ab84c70a4b56122eec09b7505
ChanRithy27 <chanrithy27@icloud.com>
2023-07-05 00:00:33 -0700

    included the api link as well as a useState to be able to display my information from the API.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [pokeData, setPokeData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(response => {setPokeData(response.data.results)})
+  }, []);
+
   return (
     <div className="App">
       <h1>Pokemon API</h1>
+      <ul>
+      {pokeData.map((pokemon, index) => (
+        <li key={index}>
+          {pokemon.name}
+        </li>
+      ))}
+      </ul>
     </div>
   );
 }
